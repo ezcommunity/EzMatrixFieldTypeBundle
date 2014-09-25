crevillo/MatrixFieldTypeBundle

First commit of Matrix FieldType

FieldType/Matrix/ColumnCollection.php
@@ -1,0 +1,129 @@
+<?php
+/**
+ * ColumnCollection for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace Blend\EzMatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+
+/**
+ * Class ColumnCollection
+ * Collection of Column values for Matrix
+ *
+ * @package Blend\EzMatrixBundle\FieldType\Matrix
+ */
+class ColumnCollection extends \ArrayObject
+{
+
+    /**
+     * @param \Blend\EzMatrixBundle\FieldType\Matrix\Column[] $elements
+     */
+    public function __construct( array $elements = array() )
+    {
+        // Call parent constructor without $elements because all column elements
+        // must be given an id by $this->offsetSet()
+        parent::__construct();
+        foreach ( $elements as $i => $author )
+        {
+            $this->offsetSet( $i, $author );
+        }
+    }
+
+    /**
+     * Create a ColumnCollection from an array of field names.
+     * The ids will be auto-generated based on the names.
+     * @param array $names
+     *
+     * @return ColumnCollection
+     */
+    public static function createFromNames( array $names = array() )
+    {
+        $columns = array();
+        $i = 1;
+
+        foreach ( $names as $name )
+        {
+            $id = strtolower(
+                preg_replace(
+                    array( '/\s+/', '/[^a-z0-9_]/i' ),
+                    array( '_', '' ),
+                    $name
+                )
+            );
+
+            $columns[] = new Column( array( 'id' => $id, 'name' => $name, 'num' => $i ) );
+            $i++;
+        }
+
+        return new self( $columns );
+    }
+
+    /**
+     * Adds a new author to the collection
+     *
+     * @throws InvalidArgumentType When $value is not of type Column
+     * @param int $offset
+     * @param \Blend\EzMatrixBundle\FieldType\Matrix\Column $value
+     */
+    public function offsetSet( $offset, $value )
+    {
+        if ( !$value instanceof Column )
+        {
+            throw new InvalidArgumentType(
+                '$value',
+                'Blend\\EzMatrixBundle\\FieldType\\Matrix\\Column',
+                $value
+            );
+        }
+
+        $aColumns = $this->getArrayCopy();
+        parent::offsetSet( $offset, $value );
+        if ( !isset( $value->id ) || $value->id == -1 )
+        {
+            if ( !empty( $aColumns ) )
+            {
+                $value->id = end( $aColumns )->id + 1;
+            }
+            else
+            {
+                $value->id = 1;
+            }
+        }
+    }
+
+    /*
+     * Returns a comma-separated list of the column names.
+     * @return string
+     */
+    public function getColumnNames()
+    {
+        return implode( ', ', $this->getArrayCopy() );
+    }
+
+    public function __toString()
+    {
+        return implode( "\t", $this->getArrayCopy() );
+    }
+
+    /**
+     * Returns an array compatible with the 'columns' portion of the Matrix hash format.
+     * @return array
+     */
+    public function toArray()
+    {
+        $columns = array();
+        foreach ( $this->getArrayCopy() as $column )
+        {
+            $columns[] = $column->toArray();
+        }
+
+        return $columns;
+    }
+}

FieldType/Matrix/Matrix.php
@@ -1,0 +1,26 @@
+<?php
+/**
+ * Value Object for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace Blend\EzMatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\SPI\Persistence\ValueObject;
+
+/**
+ * Class Matrix
+ * Value object for the Matrix FieldType
+ *
+ * @package Blend\EzMatrixBundle\FieldType\Matrix
+ */
+class Matrix extends ValueObject {
+    public $id;
+    public $columns;
+    public $rows;
+} 

FieldType/Matrix/Row.php
@@ -1,0 +1,59 @@
+<?php
+/**
+ * Row Object for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace Blend\EzMatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\SPI\Persistence\ValueObject;
+
+/**
+ * Class Row
+ * Represents a Row in a Matrix
+ *
+ * @package Blend\EzMatrixBundle\FieldType\Matrix
+ */
+class Row extends ValueObject
+{
+
+    /**
+     * Row's index within the row collection.
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Associative array of column values
+     *
+     * @var array
+     */
+    public $columns;
+
+    public function __construct( array $columns )
+    {
+        $this->columns = $columns;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return implode( "\t", $this->columns );
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->columns;
+    }
+}

FieldType/Matrix/RowCollection.php
@@ -1,0 +1,100 @@
+<?php
+/**
+ * RowCollection for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace Blend\EzMatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+
+/**
+ * Class RowCollection
+ * Collection of Row values for Matrix
+ *
+ * @package Blend\EzMatrixBundle\FieldType\Matrix
+ */
+class RowCollection extends \ArrayObject
+{
+
+    /**
+     * List of all unique column identifiers in the stored rows
+     * @var array
+     */
+    public $columnIds = array();
+
+    /**
+     * @param \Blend\EzMatrixBundle\FieldType\Matrix\Row[] $elements
+     */
+    public function __construct( array $elements = array() )
+    {
+        // Call parent constructor without $elements because all Row elements
+        // must be given an id by $this->offsetSet()
+        parent::__construct();
+        foreach ( $elements as $i => $row )
+        {
+            $this->offsetSet( $i, $row );
+        }
+    }
+
+    /**
+     * Adds a new rpw to the collection
+     *
+     * @throws InvalidArgumentType When $value is not of type Row
+     * @param int $offset
+     * @param \Blend\EzMatrixBundle\FieldType\Matrix\Row $value
+     */
+    public function offsetSet( $offset, $value )
+    {
+        if ( !$value instanceof Row )
+        {
+            throw new InvalidArgumentType(
+                '$value',
+                'Blend\\EzMatrixBundle\\FieldType\\Matrix\\Row',
+                $value
+            );
+        }
+
+        $aRows = $this->getArrayCopy();
+        parent::offsetSet( $offset, $value );
+        $this->columnIds = array_merge( $this->columnIds, array_keys( $value->columns ) );
+        if ( !isset( $value->id ) || $value->id == -1 )
+        {
+            if ( !empty( $aRows ) )
+            {
+                $value->id = end( $aRows )->id + 1;
+            }
+            else
+            {
+                $value->id = 1;
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return implode( "\n", $this->getArrayCopy() );
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $rows = array();
+        foreach ( $this->getArrayCopy() as $row )
+        {
+            $rows[] = $row->toArray();
+        }
+
+        return $rows;
+    }
+}

Tests/MatrixConverterTest.php
@@ -1,0 +1,200 @@
+<?php
+/**
+ * Test for the Matrix storage converter
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace Blend\EzMatrixBundle\Tests;
+
+use eZ\Publish\SPI\Persistence\Content\FieldValue;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue;
+use Blend\EzMatrixBundle\Persistence\Legacy\Content\FieldValue\Converter\Matrix as MatrixConverter;
+use PHPUnit_Framework_TestCase;
+use DOMDocument;
+
+class MatrixConverterTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Blend\EzMatrixBundle\Persistence\Legacy\Content\FieldValue\Converter\Matrix
+     */
+    protected $converter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->converter = new MatrixConverter();
+    }
+
+    protected function tearDown()
+    {
+        unset( $this->converter );
+        parent::tearDown();
+    }
+
+    protected function getMultiRowMatrixHash()
+    {
+        return array(
+            'rows' => array(
+                array(
+                    'name' => 'Lancelot',
+                    'quest' => 'Grail',
+                    'colour' => 'blue'
+                ),
+                array(
+                    'name' => 'Gallahad',
+                    'quest' => 'Seek Grail',
+                    'colour' => 'Blue! no, Red! Augh!'
+                )
+            ),
+            'columns' => array(
+                array(
+                    'id' => 'name',
+                    'name' => 'name',
+                    'num' => 1
+                ),
+                array(
+                    'id' => 'quest',
+                    'name' => 'quest',
+                    'num' => 2
+                ),
+                array(
+                    'id' => 'colour',
+                    'name' => 'colour',
+                    'num' => 3
+                )
+            )
+        );
+    }
+
+    public function testToStorageValue()
+    {
+        $matrixHash = $this->getMultiRowMatrixHash();
+        $value = new FieldValue;
+        $value->data = $matrixHash;
+        $storageFieldValue = new StorageFieldValue;
+
+        $this->converter->toStorageValue( $value, $storageFieldValue );
+        $doc = new DOMDocument( '1.0', 'utf-8' );
+        self::assertTrue( $doc->loadXML( $storageFieldValue->dataText ) );
+
+        self::assertEquals( $doc->documentElement->tagName, 'ezmatrix' );
+
+        $columnsNodes = $doc->getElementsByTagName( 'columns' );
+
+        $colSize = count( $matrixHash['columns'] );
+
+        self::assertEquals( 1, $columnsNodes->length );
+        self::assertEquals(
+            $colSize,
+            (int)$columnsNodes->item( 0 )->attributes->getNamedItem( 'number' )->nodeValue
+        );
+
+        foreach ( $doc->getElementsByTagName( 'column' ) as $i => $columnNode )
+        {
+            $column = $matrixHash['columns'][$i];
+            self::assertEquals( $column['id'], $columnNode->getAttribute( 'id' ) );
+            self::assertEquals( $column['num'], $columnNode->getAttribute( 'num' ) );
+            self::assertEquals( $column['name'], $columnNode->textContent );
+        }
+
+        $rowsNodes = $doc->getElementsByTagName( 'rows' );
+
+        $rowSize = count( $matrixHash['rows'] );
+
+        self::assertEquals( 1, $rowsNodes->length );
+        self::assertEquals(
+            $rowSize,
+            (int)$rowsNodes->item( 0 )->attributes->getNamedItem( 'number' )->nodeValue
+        );
+
+        foreach ( $doc->getElementsByTagName( 'c' ) as $i => $cNode )
+        {
+            //Select the correct row, values only
+            $row = array_values( $matrixHash['rows'][floor( $i / $colSize )] );
+            self::assertEquals( $row[ $i % $colSize], $cNode->textContent );
+        }
+
+    }
+
+    public function testToFieldValue()
+    {
+        $storageFieldValue = new StorageFieldValue;
+        $storageFieldValue->dataText = <<<EOT
+<?xml version="1.0" encoding="utf-8"?>
+<ezmatrix>
+	<columns number="3">
+		<column id="name" num="1">name</column>
+		<column id="quest" num="2">quest</column>
+		<column id="colour" num="3">colour</column>
+	</columns>
+	<rows number="2">
+		<c>Lancelot</c>
+		<c>Grail</c>
+		<c>blue</c>
+		<c>Gallahad</c>
+		<c>Seek Grail</c>
+		<c>Blue! no, Red! Augh!</c>
+	</rows>
+</ezmatrix>
+EOT;
+
+        $doc = new DOMDocument( '1.0', 'utf-8' );
+        self::assertTrue( $doc->loadXML( $storageFieldValue->dataText ) );
+
+        $fieldValue = new FieldValue();
+
+        $this->converter->toFieldValue( $storageFieldValue, $fieldValue );
+
+        self::assertInternalType( 'array', $fieldValue->data );
+
+        $matrixHash = $fieldValue->data;
+
+        $columnNodes = $doc->getElementsByTagName( 'column' );
+
+        $colSize = count( $matrixHash['columns'] );
+
+        self::assertEquals( $colSize, $columnNodes->length );
+
+        foreach ( $matrixHash['columns'] as $i => $column )
+        {
+            $columnNode = $columnNodes->item( $i );
+
+            self::assertEquals(
+                $columnNode->textContent,
+                $column['name']
+            );
+
+            self::assertEquals(
+                $columnNode->attributes->getNamedItem( 'id' )->nodeValue,
+                $column['id']
+            );
+
+            self::assertEquals(
+                $columnNode->attributes->getNamedItem( 'num' )->nodeValue,
+                $column['num']
+            );
+        }
+
+        $cNodes = $doc->getElementsByTagName( 'c' );
+
+        foreach ( $cNodes as $i => $cNode )
+        {
+            //Get the correct row
+            $rowNum = (int)floor( $i / $colSize );
+            $rowKeys = array_keys( $matrixHash['rows'][$rowNum] );
+            $rowValues = array_values( $matrixHash['rows'][$rowNum] );
+
+            self::assertEquals( $cNode->textContent, $rowValues[$i % $colSize] );
+            self::assertEquals(
+                $columnNodes->item( $i % $colSize )->attributes->getNamedItem( 'id' )->nodeValue,
+                $rowKeys[ $i % $colSize ]
+            );
+        }
+    }
+}

ezcommunity/EzMatrixFieldTypeBundle

FieldType/Matrix/ColumnCollection.php
@@ -1,0 +1,129 @@
+<?php
+/**
+ * ColumnCollection for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace EzSystems\MatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+
+/**
+ * Class ColumnCollection
+ * Collection of Column values for Matrix
+ *
+ * @package EzSystems\MatrixBundle\FieldType\Matrix
+ */
+class ColumnCollection extends \ArrayObject
+{
+
+    /**
+     * @param \EzSystems\MatrixBundle\FieldType\Matrix\Column[] $elements
+     */
+    public function __construct( array $elements = array() )
+    {
+        // Call parent constructor without $elements because all column elements
+        // must be given an id by $this->offsetSet()
+        parent::__construct();
+        foreach ( $elements as $i => $author )
+        {
+            $this->offsetSet( $i, $author );
+        }
+    }
+
+    /**
+     * Create a ColumnCollection from an array of field names.
+     * The ids will be auto-generated based on the names.
+     * @param array $names
+     *
+     * @return ColumnCollection
+     */
+    public static function createFromNames( array $names = array() )
+    {
+        $columns = array();
+        $i = 1;
+
+        foreach ( $names as $name )
+        {
+            $id = strtolower(
+                preg_replace(
+                    array( '/\s+/', '/[^a-z0-9_]/i' ),
+                    array( '_', '' ),
+                    $name
+                )
+            );
+
+            $columns[] = new Column( array( 'id' => $id, 'name' => $name, 'num' => $i ) );
+            $i++;
+        }
+
+        return new self( $columns );
+    }
+
+    /**
+     * Adds a new author to the collection
+     *
+     * @throws InvalidArgumentType When $value is not of type Column
+     * @param int $offset
+     * @param \EzSystems\MatrixBundle\FieldType\Matrix\Column $value
+     */
+    public function offsetSet( $offset, $value )
+    {
+        if ( !$value instanceof Column )
+        {
+            throw new InvalidArgumentType(
+                '$value',
+                'EzSystems\\MatrixBundle\\FieldType\\Matrix\\Column',
+                $value
+            );
+        }
+
+        $aColumns = $this->getArrayCopy();
+        parent::offsetSet( $offset, $value );
+        if ( !isset( $value->id ) || $value->id == -1 )
+        {
+            if ( !empty( $aColumns ) )
+            {
+                $value->id = end( $aColumns )->id + 1;
+            }
+            else
+            {
+                $value->id = 1;
+            }
+        }
+    }
+
+    /*
+     * Returns a comma-separated list of the column names.
+     * @return string
+     */
+    public function getColumnNames()
+    {
+        return implode( ', ', $this->getArrayCopy() );
+    }
+
+    public function __toString()
+    {
+        return implode( "\t", $this->getArrayCopy() );
+    }
+
+    /**
+     * Returns an array compatible with the 'columns' portion of the Matrix hash format.
+     * @return array
+     */
+    public function toArray()
+    {
+        $columns = array();
+        foreach ( $this->getArrayCopy() as $column )
+        {
+            $columns[] = $column->toArray();
+        }
+
+        return $columns;
+    }
+}

FieldType/Matrix/Matrix.php
@@ -1,0 +1,27 @@
+<?php
+/**
+ * Value Object for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace EzSystems\MatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\SPI\Persistence\ValueObject;
+
+/**
+ * Class Matrix
+ * Value object for the Matrix FieldType
+ *
+ * @package EzSystems\MatrixBundle\FieldType\Matrix
+ */
+class Matrix extends ValueObject
+{
+    public $id;
+    public $columns;
+    public $rows;
+} 

FieldType/Matrix/Row.php
@@ -1,0 +1,59 @@
+<?php
+/**
+ * Row Object for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace EzSystems\MatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\SPI\Persistence\ValueObject;
+
+/**
+ * Class Row
+ * Represents a Row in a Matrix
+ *
+ * @package EzSystems\MatrixBundle\FieldType\Matrix
+ */
+class Row extends ValueObject
+{
+
+    /**
+     * Row's index within the row collection.
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Associative array of column values
+     *
+     * @var array
+     */
+    public $columns;
+
+    public function __construct( array $columns )
+    {
+        $this->columns = $columns;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return implode( "\t", $this->columns );
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->columns;
+    }
+}

FieldType/Matrix/RowCollection.php
@@ -1,0 +1,100 @@
+<?php
+/**
+ * RowCollection for Matrix FieldType
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace EzSystems\MatrixBundle\FieldType\Matrix;
+
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+
+/**
+ * Class RowCollection
+ * Collection of Row values for Matrix
+ *
+ * @package EzSystems\MatrixBundle\FieldType\Matrix
+ */
+class RowCollection extends \ArrayObject
+{
+
+    /**
+     * List of all unique column identifiers in the stored rows
+     * @var array
+     */
+    public $columnIds = array();
+
+    /**
+     * @param \EzSystems\MatrixBundle\FieldType\Matrix\Row[] $elements
+     */
+    public function __construct( array $elements = array() )
+    {
+        // Call parent constructor without $elements because all Row elements
+        // must be given an id by $this->offsetSet()
+        parent::__construct();
+        foreach ( $elements as $i => $row )
+        {
+            $this->offsetSet( $i, $row );
+        }
+    }
+
+    /**
+     * Adds a new rpw to the collection
+     *
+     * @throws InvalidArgumentType When $value is not of type Row
+     * @param int $offset
+     * @param \EzSystems\MatrixBundle\FieldType\Matrix\Row $value
+     */
+    public function offsetSet( $offset, $value )
+    {
+        if ( !$value instanceof Row )
+        {
+            throw new InvalidArgumentType(
+                '$value',
+                'EzSystems\\MatrixBundle\\FieldType\\Matrix\\Row',
+                $value
+            );
+        }
+
+        $aRows = $this->getArrayCopy();
+        parent::offsetSet( $offset, $value );
+        $this->columnIds = array_merge( $this->columnIds, array_keys( $value->columns ) );
+        if ( !isset( $value->id ) || $value->id == -1 )
+        {
+            if ( !empty( $aRows ) )
+            {
+                $value->id = end( $aRows )->id + 1;
+            }
+            else
+            {
+                $value->id = 1;
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return implode( "\n", $this->getArrayCopy() );
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $rows = array();
+        foreach ( $this->getArrayCopy() as $row )
+        {
+            $rows[] = $row->toArray();
+        }
+
+        return $rows;
+    }
+}

Tests/MatrixConverterTest.php
@@ -1,0 +1,200 @@
+<?php
+/**
+ * Test for the Matrix storage converter
+ * User: joe
+ * Date: 12/12/13
+ * Time: 8:59 PM
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace EzSystems\MatrixBundle\Tests;
+
+use eZ\Publish\SPI\Persistence\Content\FieldValue;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue;
+use EzSystems\MatrixBundle\Persistence\Legacy\Content\FieldValue\Converter\Matrix as MatrixConverter;
+use PHPUnit_Framework_TestCase;
+use DOMDocument;
+
+class MatrixConverterTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \EzSystems\MatrixBundle\Persistence\Legacy\Content\FieldValue\Converter\Matrix
+     */
+    protected $converter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->converter = new MatrixConverter();
+    }
+
+    protected function tearDown()
+    {
+        unset( $this->converter );
+        parent::tearDown();
+    }
+
+    protected function getMultiRowMatrixHash()
+    {
+        return array(
+            'rows' => array(
+                array(
+                    'name' => 'Lancelot',
+                    'quest' => 'Grail',
+                    'colour' => 'blue'
+                ),
+                array(
+                    'name' => 'Gallahad',
+                    'quest' => 'Seek Grail',
+                    'colour' => 'Blue! no, Red! Augh!'
+                )
+            ),
+            'columns' => array(
+                array(
+                    'id' => 'name',
+                    'name' => 'name',
+                    'num' => 1
+                ),
+                array(
+                    'id' => 'quest',
+                    'name' => 'quest',
+                    'num' => 2
+                ),
+                array(
+                    'id' => 'colour',
+                    'name' => 'colour',
+                    'num' => 3
+                )
+            )
+        );
+    }
+
+    public function testToStorageValue()
+    {
+        $matrixHash = $this->getMultiRowMatrixHash();
+        $value = new FieldValue;
+        $value->data = $matrixHash;
+        $storageFieldValue = new StorageFieldValue;
+
+        $this->converter->toStorageValue( $value, $storageFieldValue );
+        $doc = new DOMDocument( '1.0', 'utf-8' );
+        self::assertTrue( $doc->loadXML( $storageFieldValue->dataText ) );
+
+        self::assertEquals( $doc->documentElement->tagName, 'ezmatrix' );
+
+        $columnsNodes = $doc->getElementsByTagName( 'columns' );
+
+        $colSize = count( $matrixHash['columns'] );
+
+        self::assertEquals( 1, $columnsNodes->length );
+        self::assertEquals(
+            $colSize,
+            (int)$columnsNodes->item( 0 )->attributes->getNamedItem( 'number' )->nodeValue
+        );
+
+        foreach ( $doc->getElementsByTagName( 'column' ) as $i => $columnNode )
+        {
+            $column = $matrixHash['columns'][$i];
+            self::assertEquals( $column['id'], $columnNode->getAttribute( 'id' ) );
+            self::assertEquals( $column['num'], $columnNode->getAttribute( 'num' ) );
+            self::assertEquals( $column['name'], $columnNode->textContent );
+        }
+
+        $rowsNodes = $doc->getElementsByTagName( 'rows' );
+
+        $rowSize = count( $matrixHash['rows'] );
+
+        self::assertEquals( 1, $rowsNodes->length );
+        self::assertEquals(
+            $rowSize,
+            (int)$rowsNodes->item( 0 )->attributes->getNamedItem( 'number' )->nodeValue
+        );
+
+        foreach ( $doc->getElementsByTagName( 'c' ) as $i => $cNode )
+        {
+            //Select the correct row, values only
+            $row = array_values( $matrixHash['rows'][floor( $i / $colSize )] );
+            self::assertEquals( $row[ $i % $colSize], $cNode->textContent );
+        }
+
+    }
+
+    public function testToFieldValue()
+    {
+        $storageFieldValue = new StorageFieldValue;
+        $storageFieldValue->dataText = <<<EOT
+<?xml version="1.0" encoding="utf-8"?>
+<ezmatrix>
+	<columns number="3">
+		<column id="name" num="1">name</column>
+		<column id="quest" num="2">quest</column>
+		<column id="colour" num="3">colour</column>
+	</columns>
+	<rows number="2">
+		<c>Lancelot</c>
+		<c>Grail</c>
+		<c>blue</c>
+		<c>Gallahad</c>
+		<c>Seek Grail</c>
+		<c>Blue! no, Red! Augh!</c>
+	</rows>
+</ezmatrix>
+EOT;
+
+        $doc = new DOMDocument( '1.0', 'utf-8' );
+        self::assertTrue( $doc->loadXML( $storageFieldValue->dataText ) );
+
+        $fieldValue = new FieldValue();
+
+        $this->converter->toFieldValue( $storageFieldValue, $fieldValue );
+
+        self::assertInternalType( 'array', $fieldValue->data );
+
+        $matrixHash = $fieldValue->data;
+
+        $columnNodes = $doc->getElementsByTagName( 'column' );
+
+        $colSize = count( $matrixHash['columns'] );
+
+        self::assertEquals( $colSize, $columnNodes->length );
+
+        foreach ( $matrixHash['columns'] as $i => $column )
+        {
+            $columnNode = $columnNodes->item( $i );
+
+            self::assertEquals(
+                $columnNode->textContent,
+                $column['name']
+            );
+
+            self::assertEquals(
+                $columnNode->attributes->getNamedItem( 'id' )->nodeValue,
+                $column['id']
+            );
+
+            self::assertEquals(
+                $columnNode->attributes->getNamedItem( 'num' )->nodeValue,
+                $column['num']
+            );
+        }
+
+        $cNodes = $doc->getElementsByTagName( 'c' );
+
+        foreach ( $cNodes as $i => $cNode )
+        {
+            //Get the correct row
+            $rowNum = (int)floor( $i / $colSize );
+            $rowKeys = array_keys( $matrixHash['rows'][$rowNum] );
+            $rowValues = array_values( $matrixHash['rows'][$rowNum] );
+
+            self::assertEquals( $cNode->textContent, $rowValues[$i % $colSize] );
+            self::assertEquals(
+                $columnNodes->item( $i % $colSize )->attributes->getNamedItem( 'id' )->nodeValue,
+                $rowKeys[ $i % $colSize ]
+            );
+        }
+    }
+}