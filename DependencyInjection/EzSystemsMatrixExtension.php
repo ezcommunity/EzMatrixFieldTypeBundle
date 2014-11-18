7ca041173e5ae9fca800bb8a7c64c8d4f1627af5
Sebastian Blank <sebastian.bl@gmx.de>
2014-11-18 18:09:58 +0100

    Fix: Configuration class doesn't exist anymore

--- DependencyInjection/EzSystemsMatrixExtension.php
+++ DependencyInjection/EzSystemsMatrixExtension.php
@@ -30,9 +30,6 @@ class EzSystemsMatrixExtension extends Extension implements PrependExtensionInte
      */
     public function load( array $configs, ContainerBuilder $container )
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
         $loader->load( 'services.yml' );
     }
